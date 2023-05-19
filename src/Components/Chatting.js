b71d86254a28f8135d9f93123c336a8668bf8e5c
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-05-19 15:49:59 +0530

    modal

--- src/Components/Chatting.js
+++ src/Components/Chatting.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from "../Images/logo.svg"
 
 const Chatting = () => {
   return (
     <div>
-    <div className="ChattingMain"></div>
+    <div className="ChattingMain">
+      <div><img src={logo} alt="" /></div>
+      <div className='joinLetter'>Connect. Chat. Collaborate.</div>
+    </div>
     </div>
   )
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import plus from "../Images/plus.svg";
+import create from "../Images/create.svg";
+import join from "../Images/join.svg";
+import cross from "../Images/cross.svg";
 
 const Sidebar = ({ room }) => {
+  const [modal, setModal] = useState(false);
   console.log(room);
   return (
     <div>
+      {modal && (
+        <div className="modal">
+          <div className="modalMain">
+            <div className="ModalHeading">
+              Create or join room now
+              <div onClick={()=>{setModal(false)}}>
+                <img src={cross} alt="" />
+              </div>
+            </div>
+            <div className="inputField">
+              <div>
+                <input type="text" placeholder="Enter Room Name..." />
+              </div>
+              <div>
+                <img src={create} alt="" />
+              </div>
+            </div>
+            <div className="or">OR</div>
+            <div className="inputField">
+              <div>
+                <input type="text" placeholder="Enter Room Link..." />
+              </div>
+              <div>
+                <img src={join} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="SidebarMain">
         {room?.length === 0 ? (
           <div className="sidebarNull">
             <div>
-              <img src={plus} alt="" />
+              <img
+                src={plus}
+                alt=""
+                onClick={() => {
+                  setModal(true);
+                }}
+              />
             </div>
             <div className="joinLetter">Create or Join Chat Rooms...</div>
           </div>
