188f5297f6c675634a49a0911e24a12107df8afe
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-05-19 14:29:03 +0530

    sidebar

---
+++ src/Components/Chatting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chatting = () => {
+  return (
+    <div>
+    <div className="ChattingMain"></div>
+    </div>
+  )
+}
+
+export default Chatting
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
-import { collection, addDoc, doc, query, getDoc, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  doc,
+  query,
+  getDoc,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../utils/firebase";
+import Sidebar from "../Components/Sidebar";
+import Chatting from "../Components/Chatting";
+import "./index.css";
 
 const Home = () => {
   const [user, loading] = useAuthState(auth);
@@ -18,40 +29,47 @@ const Home = () => {
   };
 
   const getData = async () => {
-    const q = query(collection(db, "userRooms"), where(
-        "users",
-        "array-contains",
-        { id: "vI0vpqYVulVXHWgqFKm6k5X2uWF2"}
-      ))
+    const q = query(
+      collection(db, "userRooms"),
+      where("users", "array-contains", { id: "vI0vpqYVulVXHWgqFKm6k5X2uWF2" })
+    );
     const docSnap = await getDocs(q);
     console.log(docSnap);
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={roomName}
-        onChange={(e) => {
-          setRoomName(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          handleSubmit();
-        }}
-      >
-        Create Room
-      </button>
-      <button
-        onClick={() => {
-          getData();
-        }}
-      >
-        get Room
-      </button>
+    <div className="HomeMain">
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <div className="Chatting">
+        <Chatting />
+      </div>
     </div>
   );
 };
 
 export default Home;
+// <div>
+//   <input
+//     type="text"
+//     value={roomName}
+//     onChange={(e) => {
+//       setRoomName(e.target.value);
+//     }}
+//   />
+//   <button
+//     onClick={() => {
+//       handleSubmit();
+//     }}
+//   >
+//     Create Room
+//   </button>
+//   <button
+//     onClick={() => {
+//       getData();
+//     }}
+//   >
+//     get Room
+//   </button>
+// </div>
