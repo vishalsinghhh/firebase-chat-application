86573ff17209acaf6a581b3f4e6ddf9b214caa81
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-05-20 17:07:44 +0530

    message css

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../utils/firebase";
 
-const Messages = ({message}) => {
-    console.log(message);
+const Messages = ({ messages }) => {
+  const [user, loading] = useAuthState(auth);
+  console.log(messages);
   return (
-    <div>Messages</div>
-  )
-}
+    <div>
+      <div>
+        {messages.senderId === user.uid ? (
+          <div className="myMessage1"><p><span className="myMessageNew">{messages.text}</span></p></div>
+        ) : (
+          <div className="myMessage2"><p><span className="myMessageNew">{messages.text}</span></p></div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Messages
+export default Messages;
